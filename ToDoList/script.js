23fe664d2c1ff6e513eb0105820fd658aefe22eb
anamika-as <anamikasuresh115@gmail.com>
2026-06-18 21:29:42 +0530

    Added Editing functionality

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,8 +1,10 @@
+
 function addTask(task){
     const toDoItem = document.createElement("li")
     const checkbox = document.createElement("input");
     const taskName = document.createElement("p");
     const delBtn = document.createElement("span");
+    const editBtn = document.createElement("span");
 
     toDoItem.className = "listItem";
     checkbox.type = "checkbox";
@@ -12,6 +14,9 @@ function addTask(task){
     delBtn.id = 'deleteBtn';
     delBtn.textContent = 'Delete';
     toDoItem.append(delBtn);
+    editBtn.id = 'editBtn';
+    editBtn.textContent = 'Edit';
+    toDoItem.append(editBtn);
 
     document.querySelector("ul").append(toDoItem);
     console.log(toDoItem);
@@ -19,11 +24,23 @@ function addTask(task){
 
 const addBtn = document.querySelector(".addBtn");
 addBtn.addEventListener("click",()=>{
-    let inputField = document.querySelector("input");
-    addTask(inputField.value);  
-    inputField.value = ""  
+    const inputField = document.querySelector("input");
+    if(!isEditingItem && inputField.value != ""){
+        addTask(inputField.value);  
+        inputField.value = ""; 
+    }
+    else{
+        const p = document.createElement("p");
+        p.textContent = inputField.value;
+        editItem.children[1].replaceWith(p);
+        inputField.value = "";
+        isEditingItem = false;
+    }
+    
 });
 
+let isEditingItem = false;
+let editItem = null;
 const toDoList = document.querySelector("ul");
 console.log(toDoList);
 toDoList.addEventListener("click",(e)=>{
@@ -31,6 +48,18 @@ toDoList.addEventListener("click",(e)=>{
         e.target.parentElement.remove();
     }
 });
+
+toDoList.addEventListener("click",(e)=>{    
+   if (e.target.id == "editBtn"){
+        const inputField = document.querySelector("input");
+        inputField.value = e.target.parentElement.children[1].textContent;
+        editItem = e.target.parentElement;
+        isEditingItem = true;
+        
+    }
+})
+
+
 
 
 
