2b285ff3820f03dc552f8ba4f8f4876f98c2f753
anamika-as <anamikasuresh115@gmail.com>
2026-06-18 16:26:11 +0530

    Seperate CSS/JS into their own files and added delete functionality

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,7 +1,36 @@
-const task = document.querySelector("input[type=text]");
-console.log(task.value);
-// document.querySelector("input[type=submit]").addEventListener("submit",addTask,false);
+function addTask(task){
+    const toDoItem = document.createElement("li")
+    const checkbox = document.createElement("input");
+    const taskName = document.createElement("p");
+    const delBtn = document.createElement("span");
 
-// function addTask(task){
-//     document.
-// }
+    toDoItem.className = "listItem";
+    checkbox.type = "checkbox";
+    toDoItem.append(checkbox);
+    taskName.textContent = task;
+    toDoItem.append(taskName);
+    delBtn.id = 'deleteBtn';
+    delBtn.textContent = 'Delete';
+    toDoItem.append(delBtn);
+
+    document.querySelector("ul").append(toDoItem);
+    console.log(toDoItem);
+}
+
+const addBtn = document.querySelector(".addBtn");
+addBtn.addEventListener("click",()=>{
+    let inputField = document.querySelector("input");
+    addTask(inputField.value);  
+    inputField.value = ""  
+});
+
+const toDoList = document.querySelector("ul");
+console.log(toDoList);
+toDoList.addEventListener("click",(e)=>{
+    if (e.target.id == "deleteBtn"){
+        e.target.parentElement.remove();
+    }
+});
+
+
+
