1553ae2bdae06dcf385b0b3ae7da3cc54e12889a
anamika-as <anamikasuresh115@gmail.com>
2026-06-18 14:33:14 +0530

    Simple To do List

---
+++ ToDoList/script.js
@@ -0,0 +1,7 @@
+const task = document.querySelector("input[type=text]");
+console.log(task.value);
+// document.querySelector("input[type=submit]").addEventListener("submit",addTask,false);
+
+// function addTask(task){
+//     document.
+// }
